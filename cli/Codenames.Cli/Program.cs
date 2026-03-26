5051d16cb748654261b8117f640eb43aff46e3dd
Massamba Maphalala <massamba.maphalala@bbd.co.za>
2026-03-26 09:19:45 +0000

    Setup CLI project (.NET 10)

---
+++ cli/Codenames.Cli/Program.cs
@@ -0,0 +1,16 @@
+﻿using Spectre.Console;
+
+namespace Codenames.Cli;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var figlet = new FigletText("Codenames CLI")
+            .Color(Color.Blue);
+  
+        AnsiConsole.Write(figlet);
+        AnsiConsole.MarkupLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
